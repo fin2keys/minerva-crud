eb821b3b23310903eabe0f95780a8e2c2b633f7f
fin2keys <67210928+fin2keys@users.noreply.github.com>
2020-06-29 22:30:32 -0300

    update 0.4

--- src/js/functions.js
+++ src/js/functions.js
@@ -10,19 +10,44 @@ const openDisplay = () => {
     addNewItemForm.style.setProperty('display', 'flex')
   }
 }
+const currentStore = []
+
+const renderAllProducts = () => {
+  const savedItens = localStorage.savedProducts ? JSON.parse(localStorage.savedProducts) : []
+
+  savedItens.forEach(car => loadSavedItens(car))
+}
+
+const loadSavedItens = itens => {
+  const mainTable = document.querySelector('.new-products-container')
+  const newRow = document.createElement('tr')
+
+  newRow.innerHTML = `
+    <th>${itens.name}</th>
+    <th>${itens.price}</th>
+    <th>${itens.discount}</th>
+    <th>DELETE</th>
+  `
+  mainTable.appendChild(newRow)
+}
 const addNewItem = () => {
   const newItem = {
     name: document.getElementById('pdc-name').value,
     price: document.getElementById('pdc-price').value,
     discount: document.getElementById('pdc-discount').value
   }
+  loadSavedItens(newItem)
   console.log(newItem)
   getById('pdc-name').value = ''
   getById('pdc-price').value = ''
   getById('pdc-discount').value = ''
 
   getById('mainform').style.setProperty('display', 'none')
-}
 
+  const savedProducts = localStorage.savedProducts ? JSON.parse(localStorage.savedProducts) : []
+  savedProducts.push(newItem)
+  localStorage.savedProducts = JSON.stringify(savedProducts)
+}
 addNewProductButton.addEventListener('click', openDisplay)
 addButton.addEventListener('click', addNewItem)
+document.onload = renderAllProducts()
