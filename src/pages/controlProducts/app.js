50fd29395ce0265255d3051c9ff588e41ba8c4d4
fin2keys <67210928+fin2keys@users.noreply.github.com>
2020-06-30 18:20:34 -0300

    remove func implement

--- src/pages/controlProducts/app.js
+++ src/pages/controlProducts/app.js
@@ -1,5 +1,6 @@
 const addNewProductButton = document.querySelector('.add-new-product-container')
 const addButton = document.querySelector('#add')
+const tableContainer = document.querySelector('.new-products-container')
 const getById = id => document.getElementById(`${id}`)
 
 const openDisplay = () => {
@@ -10,10 +11,12 @@ const openDisplay = () => {
     addNewItemForm.style.setProperty('display', 'flex')
   }
 }
+
 const currentStore = []
 
 const renderAllProducts = () => {
-  const savedItens = localStorage.savedProducts ? JSON.parse(localStorage.savedProducts) : []
+  const savedItens = JSON.parse(localStorage.savedProducts)
+  console.log(savedItens)
 
   savedItens.forEach(car => loadSavedItens(car))
 }
@@ -26,10 +29,30 @@ const loadSavedItens = itens => {
     <th>${itens.name}</th>
     <th>${itens.price}</th>
     <th>${itens.discount}</th>
-    <th>DELETE</th>
+    <th class="delete-button" data-remove="${mainTable.childElementCount}">DELETE</th>
   `
+  newRow.setAttribute('data-remove', `${mainTable.childElementCount}`)
+  newRow.setAttribute('class', 'new-row')
   mainTable.appendChild(newRow)
 }
+const removeItemDisplay = ({target}) => {
+  const allRows = document.querySelectorAll('.new-row')
+  if(target.className === 'delete-button') {
+    allRows.forEach((element, index) => {
+      if(element.dataset.remove == target.dataset.remove) {
+        const savedItens = JSON.parse(localStorage.savedProducts)
+        removeItemStorage(savedItens, index)
+        element.remove()
+      }
+    })
+  }
+}
+
+const removeItemStorage = (item, index) => {
+  item.splice(index, 1)
+  localStorage.savedProducts = JSON.stringify(item)
+}
+
 const addNewItem = () => {
   const newItem = {
     name: document.getElementById('pdc-name').value,
@@ -50,4 +73,6 @@ const addNewItem = () => {
 }
 addNewProductButton.addEventListener('click', openDisplay)
 addButton.addEventListener('click', addNewItem)
+tableContainer.addEventListener('click', removeItemDisplay)
+
 document.onload = renderAllProducts()
